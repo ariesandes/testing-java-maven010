e2123b8588fa2ec1ac60cc1cf8661510a61671c8
ariesandes andes <ariesandes@gmail.com>
2020-03-09 23:38:58 -0300

    Primer Commit and Push.

---
+++ maven010_git/src/test/java/cl/com/mp/maven010_git/AppTest.java
@@ -0,0 +1,20 @@
+package cl.com.mp.maven010_git;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
